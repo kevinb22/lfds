dae72801968061eb3c4ad826ceee273ed3ae68cb
Anirban Biswas <anirban@cs.washington.edu>
2019-03-11 21:35:14 -0700

    Implemented lock free hash table

--- c/src/lf_map.c
+++ c/src/lf_map.c
@@ -1,25 +1,29 @@
 #include "lf_map.h"
+#include <stdatomic.h>
+#include <stdio.h>
 
 #define HT_SIZE 97
 
 hm_bucket *hash_table;
 
-int lf_map_create()
-{
+int lf_map_create() {
     hash_table = (hm_bucket *)calloc(HT_SIZE, sizeof(hm_bucket));
-    if (!hash_table)
-    {
+    if (!hash_table) {
         return -1;
+    }
+    int i;
+    for (i = 0; i < HT_SIZE; i++) {
+        hash_table[i].version = 1;
+        __sync_lock_release(&hash_table[i].spin_flag);
     }
     return 0;
 }
 
-void lf_map_put(hm_entry *entry)
-{
-    if (entry == NULL)
-    {
+void lf_map_put(hm_entry *entry) {
+    if (entry == NULL) {
         return;
     }
+    int version_old;
     unsigned long hash_key = hash(entry->key);
     // printf("PUT: key: %s, hash_val: %lu\n", entry->key, hash_key);
 
@@ -29,44 +33,93 @@ void lf_map_put(hm_entry *entry)
     // atomic
     hm_bucket *bucket = &hash_table[index];
     // Look at the version...
+    // int version_old = // atomic read. If -1, fail
+
+LOOP:
+    version_old = bucket->version;
+    // if (version_old == 0) {
+    //     goto LOOP;
+    // }
 
     lf_node *head = bucket->list;
     lf_node *next = head;
     while (next) {
-        if (hm_entry_key_equals(entry->key, ((hm_entry *) next->data)->key)) {
-
+        if (hm_entry_key_equals(entry->key, ((hm_entry *)next->data)->key)) {
             // check version number
-            ((hm_entry *) next->data)->val = entry->val;
+            // if (bucket->version == 0) {
+            //     goto LOOP;
+            // }
+            if (!__sync_bool_compare_and_swap(&bucket->spin_flag, 0, 1)) {
+                goto LOOP;
+            }
+            if (!__sync_bool_compare_and_swap(&bucket->version, version_old, version_old + 1)) {
+                if (!__sync_bool_compare_and_swap(&bucket->spin_flag, 1, 0)) {
+                    printf("Trying to release unheld lock...exiting\n");
+                    exit(1);
+                }
+                goto LOOP;
+            }
+
+            ((hm_entry *)next->data)->val = entry->val;
+            if (!__sync_bool_compare_and_swap(&bucket->spin_flag, 1, 0)) {
+                printf("Trying to release unheld lock...exiting\n");
+                exit(1);
+            }
             return;
         }
         next = next->next;
     }
 
-    lf_node *new_head = (lf_node *) malloc(sizeof(lf_node));
+    lf_node *new_head = (lf_node *)malloc(sizeof(lf_node));
     new_head->data = entry;
     new_head->next = head;
+    if (!__sync_bool_compare_and_swap(&bucket->spin_flag, 0, 1)) {
+        free(new_head);
+        goto LOOP;
+    }
+    if (!__sync_bool_compare_and_swap(&bucket->version, version_old, version_old + 1)) {
+        if (!__sync_bool_compare_and_swap(&bucket->spin_flag, 1, 0)) {
+            printf("Trying to release unheld lock...exiting\n");
+            free(new_head);
+            exit(1);
+        }
+        free(new_head);
+        goto LOOP;
+    }
     bucket->list = new_head;
+    if (!__sync_bool_compare_and_swap(&bucket->spin_flag, 1, 0)) {
+        printf("Trying to release unheld lock...exiting\n");
+        free(new_head);
+        exit(1);
+    }
 }
 
-int lf_map_get(hm_entry *entry)
-{
+int lf_map_get(hm_entry *entry) {
     if (!entry) {
         return -1;
     }
-
+    int version_old;
+    int version_new;
     unsigned long hash_key = hash(entry->key);
     // printf("GET: key: %s, hash_val: %lu\n", entry->key, hash_key);
     unsigned long index = hash_key % HT_SIZE;
 
     // atomic
-    hm_bucket bucket = hash_table[index];
+    hm_bucket *bucket = &hash_table[index];
 
-    lf_node *curr = bucket.list;
+LOOP:
+    version_old = bucket->version;
+
+    lf_node *curr = bucket->list;
 
     while (curr) {
-        if (hm_entry_key_equals(entry->key, ((hm_entry *) curr->data)->key)) {
+        __atomic_load(&bucket->version, &version_new, __ATOMIC_RELAXED);
+        if (version_old != version_new) {
+            goto LOOP;
+        }
+        if (hm_entry_key_equals(entry->key, ((hm_entry *)curr->data)->key)) {
             // check version number
-            return ((hm_entry *) curr->data)->val;
+            return ((hm_entry *)curr->data)->val;
         }
         curr = curr->next;
     }
@@ -83,14 +136,13 @@ void lf_map_destroy() {
             free(curr_node->data);
             lf_node *old_node = curr_node;
             curr_node = curr_node->next;
-            free (old_node);
+            free(old_node);
         }
     }
     free(hash_table);
 }
 
-unsigned long hash(unsigned char *str)
-{
+unsigned long hash(unsigned char *str) {
     unsigned long hash_code = 5381;
     int c;
 
--- c/src/lf_map_test.c
+++ c/src/lf_map_test.c
@@ -1,23 +1,23 @@
-#include <assert.h>
-#include <stdio.h>
 #include "lf_map.h"
-
+#include <assert.h>
+#include <pthread.h>
+#include <stdio.h>
 
 #define NUM_ENTRIES 26
+#define NUM_THREADS 16
 
-void main() {
+void test_single_threaded() {
+    printf("Testing single threaded...\n");
     lf_map_create();
 
     hm_entry *entries[NUM_ENTRIES];
     int i;
     for (i = 0; i < NUM_ENTRIES; i++) {
         hm_entry *curr_entry = (hm_entry *)malloc(sizeof(hm_entry));
-        char *key = (char *) malloc(2 * sizeof(char)); 
-        key[0]= 'a' + i;
-        key[1]= '\0';
+        char *key = (char *)malloc(2 * sizeof(char));
+        key[0] = 'a' + i;
+        key[1] = '\0';
         curr_entry->key = key;
-        // curr_entry->key[0]++;
-        // printf("i: %d key:%s\n", i, curr_entry->key);
         curr_entry->val = i;
         entries[i] = curr_entry;
         lf_map_put(curr_entry);
@@ -25,7 +25,6 @@ void main() {
 
     for (i = 0; i < NUM_ENTRIES; i++) {
         int curr_val = lf_map_get(entries[i]);
-        // printf("curr_val: %d, expected: %d\n", curr_val, i);
         assert(curr_val == i);
     }
 
@@ -33,5 +32,61 @@ void main() {
     lf_map_put(entries[0]);
     assert(lf_map_get(entries[0]) == 99);
     lf_map_destroy();
-    printf("Passed\n");
+    printf("Passed single threaded\n");
+}
+
+void *thread_start(void *args) {
+    int thread_id = (int)args;
+
+    hm_entry *entries[NUM_ENTRIES];
+    int i;
+    for (i = 0; i < NUM_ENTRIES; i++) {
+        hm_entry *curr_entry = (hm_entry *)malloc(sizeof(hm_entry));
+        char *key = (char *)malloc(2 * sizeof(char));
+        key[0] = 'a' + i;
+        key[1] = '\0';
+        curr_entry->key = key;
+        curr_entry->val = i;
+        entries[i] = curr_entry;
+        lf_map_put(curr_entry);
+    }
+
+    for (i = 0; i < NUM_ENTRIES; i++) {
+        int curr_val = lf_map_get(entries[i]);
+        if (curr_val != i) {
+            printf("i: %d curr_val: %d\n", i, curr_val);
+        }
+        assert(curr_val == i);
+    }
+
+    pthread_exit(0);
+}
+
+void test_multi_threaded() {
+    printf("Testing multi threaded...\n");
+    lf_map_create();
+    pthread_t thread_arr[NUM_THREADS];
+    int i;
+    for (i = 0; i < NUM_THREADS; i++) {
+        pthread_create(&thread_arr[i], NULL, thread_start, (void *)i);
+    }
+
+    int err = 0;
+    bool fail = false;
+    for (i = 0; i < NUM_THREADS; i++) {
+        pthread_join(thread_arr[i], (void **)&err);
+        fail |= err;
+    }
+
+    if (fail) {
+        printf("Failed multi threaded\n");
+    } else {
+        printf("Passed multi threaded\n");
+    }
+    lf_map_destroy();
+}
+
+void main() {
+    test_single_threaded();
+    test_multi_threaded();
 }
